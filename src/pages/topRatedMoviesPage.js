c336913df656c824d270d88c34365a8812b0d70b
LuoYang <luoyangyuli2001@gmail.com>
2022-11-06 17:38:37 +0000

    TopRatedMoviesPage is completed.

--- src/pages/topRatedMoviesPage.js
+++ src/pages/topRatedMoviesPage.js
@@ -1,8 +1,34 @@
 import React from "react";
+import { getTopRatedMovies } from "../api/tmdb-api";
+import PageTemplate from '../components/templateMovieListPage';
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
 
 const TopRatedMoviesPage = () => {
+  const { data, error, isLoading, isError }  = useQuery('topRated', getTopRatedMovies)
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
+
+  // Redundant, but necessary to avoid app crashing.
+  const favorites = movies.filter(m => m.favorite)
+  localStorage.setItem('favorites', JSON.stringify(favorites))
+
   return (
-    <p>test</p>
+    <PageTemplate
+      title="Top Rated Movies"
+      movies={movies}
+      action={(movie) => {
+        return <AddToFavoritesIcon movie={movie} />
+      }}
+    />
   );
 };
 
