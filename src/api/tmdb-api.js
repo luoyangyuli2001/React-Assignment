90642a6faa4aad6074d7c7444186d91fcc1ddd99
LuoYang <luoyangyuli2001@gmail.com>
2022-11-06 19:11:25 +0000

    Added Pagination on upComingMoviesPage.

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -12,9 +12,11 @@ export const getMovies = () => {
   });
 };
 
-export const getUpcomingMovies = () => {
+export const getUpcomingMovies = (args) => {
+  const [, pageNum] = args.queryKey;
+  const { page } = pageNum;
   return fetch(
-    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=${page}`
   ).then((response) => {
     if (!response.ok) {
       throw new Error(response.json().message);
--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
 import PageTemplate from "../components/templateMovieListPage";
 import { getUpcomingMovies } from "../api/tmdb-api";
 import AddToToWatchIcon from '../components/cardIcons/addToToWatch'
+import Pagination from '@mui/material/Pagination';
 
 const UpcomingMoviesPage = () => {
-  const { data, error, isLoading, isError }  = useQuery('upComing', getUpcomingMovies)
+  const [page, setPage] = useState(1);
+  const handleChange = (e, p) => {
+    setPage(p)
+  }
+  const { data, error, isLoading, isError }  = useQuery(
+    ['upComing', {page}],
+    getUpcomingMovies
+  );
 
   if (isLoading) {
     return <Spinner />
@@ -19,13 +27,42 @@ const UpcomingMoviesPage = () => {
   const movies = data.results;
 
   return (
-    <PageTemplate
-      title="Upcoming Movies"
-      movies={movies}
-      action={(movie) => {
-        return <AddToToWatchIcon movie={movie} />
-      }}
-    />
+    <>
+      <PageTemplate
+        title="Upcoming Movies"
+        movies={movies}
+        action={(movie) => {
+          return <AddToToWatchIcon movie={movie} />
+        }}
+      />   
+      <div style={{    
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      color: "white",}}
+    >
+      <div style={{
+        position: "fixed",
+        bottom: 0,
+        zIndex: 200,
+        background: "green",
+        padding: "10px 80px",
+        color: "white",
+        width: "100%",
+      }}>
+        <Pagination 
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }} 
+        variant='outlined' 
+        count={10}
+        page={page}
+        onChange={handleChange}
+        />
+      </div>
+    </div>
+    </>
   );
 };
 
